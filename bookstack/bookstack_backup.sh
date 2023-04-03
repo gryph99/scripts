7dccff48ef82c65f0c39d311ff5fb9c139239aa4
Alex Thompson <alex@ares.local>
2023-04-02 22:07:13 -0400

    created bookstack backup script

---
+++ bookstack/bookstack_backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create bookstack DB dump file
+mysqldump -u [db_user] -p [user_password] bookstack > bookstack.backup.sql
+
+# Backup bookstack files
+tar -czvf bookstack-files-backup.tar.gz /var/www/bookstack/.env /var/www/bookstack/public/uploads /var/www/bookstack/storage/uploads /var/www/bookstack/themes
+
+#Combine and compress DB and file backups into a tar. 
+tar -czvf bookstack-backup.tar.gz bookstack-files-backup.tar.gz kb.alexthompson.tech.backup.sql 
+
+# Clean up uneeded files.
+rm bookstack-files-backup.tar.gz kb.alexthompson.tech.backup.sql
+
+echo "Backup Complete"
