474f6345980a84c8ac1e76276db59301fa7bccd7
Gryph99 <gryphon68@protonmail.com>
2023-04-02 22:14:03 -0400

    created readme for bookstack-backup

---
+++ bookstack/bookstack-backup/README.md
@@ -0,0 +1,7 @@
+# Bookstack Backup Script
+
+A quick script for backing up the custom data uploaded to your bookstack instance following the recommendations here: https://www.bookstackapp.com/docs/admin/backup-restore/
+
+## Before running this script
+
+1. Set Database username and passwordS
--- bookstack/bookstack-backup/bookstack_backup.sh
+++ bookstack/bookstack-backup/bookstack_backup.sh
@@ -7,9 +7,9 @@ mysqldump -u [db_user] -p [user_password] bookstack > bookstack.backup.sql
 tar -czvf bookstack-files-backup.tar.gz /var/www/bookstack/.env /var/www/bookstack/public/uploads /var/www/bookstack/storage/uploads /var/www/bookstack/themes
 
 #Combine and compress DB and file backups into a tar. 
-tar -czvf bookstack-backup.tar.gz bookstack-files-backup.tar.gz kb.alexthompson.tech.backup.sql 
+tar -czvf bookstack-backup.tar.gz bookstack-files-backup.tar.gz bookstack.backup.sql 
 
 # Clean up uneeded files.
-rm bookstack-files-backup.tar.gz kb.alexthompson.tech.backup.sql
+rm bookstack-files-backup.tar.gz bookstack.backup.sql
 
 echo "Backup Complete"
